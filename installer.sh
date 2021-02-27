497c077505fd5e35bc66ebdb211c051b800e44bc
anonymous <45884841+sahil-kazama@users.noreply.github.com>
2021-02-28 02:16:08 +0530

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+cmd=cmd
+cmd=execute
+clear
+reset
+
+sudo cp gdrive /bin/
+echo "  [INSTALL] ~> Installing Linux Gaming Toolkit....  
+"
+sleep 5s
+gdrive https://drive.google.com/file/d/1ykQySLoq_Kdf1OOrOh0R_wq_IhyUf0ei/view?usp=sharing
+7z x *.img -o*
+rm *.img
