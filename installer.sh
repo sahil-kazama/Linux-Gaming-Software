81e541403455c0db134549f35ba61ec1ab9ffc42
anonymous <45884841+sahil-kazama@users.noreply.github.com>
2021-03-01 02:20:23 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,4 +11,3 @@ echo "  [INSTALL] ~> Installing Linux Gaming Toolkit....
 sleep 5s
 gdrive https://drive.google.com/file/d/1ykQySLoq_Kdf1OOrOh0R_wq_IhyUf0ei/view?usp=sharing
 7z x *.img -o*
-rm *.img
