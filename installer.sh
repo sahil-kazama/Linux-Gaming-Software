e753938ef04ccf4e72ed7ac06c7c921a47a095a6
anonymous <45884841+sahil-kazama@users.noreply.github.com>
2021-03-29 23:23:43 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,5 +9,5 @@ sudo cp gdrive /bin/
 echo "  [INSTALL] ~> Installing Linux Gaming Toolkit....  
 "
 sleep 5s
-gdrive https://drive.google.com/file/d/1TXbNY0G8iHMDvLoOhnxCuR4s5rvpmUNG/view?usp=sharing
+gdrive https://drive.google.com/file/d/1iYLfqOXaAfAGHpRHlLLVXXUf2mGLr6TT/view?usp=sharing
 unsquashfs -d Linux-Pro-Gaming-Toolkit Linux-Pro-Gaming-Toolkit.img
