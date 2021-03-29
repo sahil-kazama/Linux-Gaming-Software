8b9b0c9e9c655b720f583053d0660bfbb6b9482c
anonymous <45884841+sahil-kazama@users.noreply.github.com>
2021-03-29 23:34:49 +0530

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -11,3 +11,4 @@ echo "  [INSTALL] ~> Installing Linux Gaming Toolkit....
 sleep 5s
 gdrive https://drive.google.com/file/d/1iYLfqOXaAfAGHpRHlLLVXXUf2mGLr6TT/view?usp=sharing
 unsquashfs -d Linux-Pro-Gaming-Toolkit Linux-Pro-Gaming-Toolkit.img
+rm Linux-Pro-Gaming-Toolkit.img
